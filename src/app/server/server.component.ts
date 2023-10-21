99991ea52c68ecb53fd9857eae70b6b3d2c4c6fb
AzureAD\XinyuZhu <xinyu.zhu@veovo.com>
2023-10-22 10:52:48 +1300

    update

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,13 @@ export class ServerComponent {
   serverId = 10;
   serverStatus: string = 'offline';
 
+  constructor(){
+    this.serverStatus = Math.random()>0.5?'online':'offline';
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
+  }
+
 }
 
 
